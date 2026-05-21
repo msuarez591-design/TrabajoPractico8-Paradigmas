b825e203791991a014c9cf5755df11b123d66f82
mmartinn02 <mmartinn02@gmail.com>
2026-05-21 00:18:09 -0300

    clase persona lista

--- Persona.java
+++ Persona.java
@@ -5,17 +5,31 @@
 public class Persona {
     private int dni;
     private String nombre;
+    private Cuenta cuentaPesos;
+    private Cuenta cuentaDolares;
 
-    /**
+     /**
      * Constructor de Persona
+     * @version 1.0
+     * @author Bravo Guadalupe, Suarez Martin
      */
-    public Persona(int dni, String nombre) {
+    public Persona(int dni, String nombre,String tipoDeMoneda, Float saldo, int numeroCuenta) {
+        if (dni <= 0) {
+            throw new IllegalArgumentException("El DNI debe ser un número positivo.");
+        }
+        if (nombre == null || nombre.trim().isEmpty()) {
+            throw new IllegalArgumentException("El nombre no puede estar vacío.");
+        }
         this.dni = dni;
         this.nombre = nombre;
+        this.cuentaPesos = new Cuenta(tipoDeMoneda, saldo, numeroCuenta);
+        this.cuentaDolares = new Cuenta(tipoDeMoneda, saldo, numeroCuenta);
     }
     public Persona() {
         this.dni = 0;
         this.nombre = "";
+        this.cuentaPesos = new Cuenta();
+        this.cuentaDolares = new Cuenta();
     }
 
     // Getters
@@ -27,6 +41,14 @@ public class Persona {
         return nombre;
     }
 
+    public Cuenta getCuentaPesos() {
+            return cuentaPesos;
+    }
+
+    public Cuenta getCuentaDolares() {
+            return cuentaDolares;
+    }
+
     // Setters
     public void setNombre(String nombre) {
         this.nombre = nombre;
@@ -34,5 +56,24 @@ public class Persona {
     
     public void setDni(int dni) {
         this.dni = dni;
+    }
+
+    public void setCuentaPesos(Cuenta cuentaPesos) {
+        this.cuentaPesos = cuentaPesos;
+    }
+
+    public void setCuentaDolares(Cuenta cuentaDolares) {
+        this.cuentaDolares = cuentaDolares;
+    }
+
+    //metodo
+    public void getCuenta(String tipoDeMoneda){
+        if(tipoDeMoneda.equalsIgnoreCase("pesos")){
+            System.out.println("Cuenta en pesos: " + cuentaPesos.getSaldo());
+        } else if(tipoDeMoneda.equalsIgnoreCase("dolares")){
+            System.out.println("Cuenta en dolares: " + cuentaDolares.getSaldo());
+        } else {
+            System.out.println("Tipo de moneda no reconocido.");
+        }
     }
 }
